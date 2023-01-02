f6c0566733770d63a1c2271f5299586a54c34f6f
ever <ever@brokenmouse.studio>
2023-01-02 21:59:19 +0100

    Added the rest of the documentation files to lcdocs_summary.json

--- developer/generate_summary.py
+++ developer/generate_summary.py
@@ -52,32 +52,30 @@ def get_unique_value(node, tag, file_path):
 
 categories = []
 
-def create_entry(entries, raw_file_path, node):
+def create_entry(entries, path, node):
 	tag_name = node.tagName
 	
-	file_path = raw_file_path.replace(os.path.sep, '/')[len(root_dir) + 1:] # replace os specific seperators with / so it's usable in an URL
-	
-	category = get_unique_value(node, 'category', file_path)
+	category = get_unique_value(node, 'category', path)
 	if category not in categories:
 		categories.append(category)
 	deprecated_tags = node.getElementsByTagName('deprecated')
 	deprecated_version = None
 	if len(deprecated_tags) > 0:
-		deprecated_version = get_unique_value(deprecated_tags[0], 'version', file_path)
+		deprecated_version = get_unique_value(deprecated_tags[0], 'version', path)
 	if tag_name == 'const':
-		name = get_unique_value(node, 'name', file_path)
-		version = get_unique_value(node, 'version', file_path)
+		name = get_unique_value(node, 'name', path)
+		version = get_unique_value(node, 'version', path)
 	elif tag_name == 'func':
-		name = get_unique_value(node, 'title', file_path)
-		version = get_unique_value(get_unique_tag(node, 'versions', file_path), 'version', file_path)
+		name = get_unique_value(node, 'title', path)
+		version = get_unique_value(get_unique_tag(node, 'versions', path), 'version', path)
 	
 	if not (name and category and version):
-		print(f'Skipping <{tag_name}> in {file_path}', file=sys.stderr)
+		print(f'Skipping <{tag_name}> in {path}', file=sys.stderr)
 		return
 	
 	# this is the object which is later exported to the summary JSON file
 	entry = {
-		'path': file_path,
+		'path': path,
 		'name': name,
 		'category': category,
 		'version': version,
@@ -88,23 +86,50 @@ def create_entry(entries, raw_file_path, node):
 # search recursively in a given path for XML files
 constants = []
 functions = []
+doc_files = {}
 for path, dir_names, files in os.walk(root_dir):
 	for file_name in files:
 		# ignore non-XML files
 		if not file_name.endswith('.xml'):
 			continue
 		
-		file_path = os.path.join(path, file_name)
-		# parse the XML file into a DOM object
-		document = minidom.parse(file_path)
+		file_path = os.path.join(path, file_name).replace(os.path.sep, '/') # replace os specific seperators with / so it's usable in an URL
+		rel_file_path = file_path[len(root_dir) + 1:] # cut off the root directory
 		
-		# find <const> tags in the current document
-		for const in document.getElementsByTagName('const'):
-			create_entry(constants, file_path, const)
-		
-		# find <func> tags in the current document
-		for func in document.getElementsByTagName('func'):
-			create_entry(functions, file_path, func)
+		if rel_file_path.startswith('script/'):
+			# parse the XML file into a DOM object
+			document = minidom.parse(file_path)
+			
+			# find <const> tags in the current document
+			for const in document.getElementsByTagName('const'):
+				create_entry(constants, rel_file_path, const)
+			
+			# find <func> tags in the current document
+			for func in document.getElementsByTagName('func'):
+				create_entry(functions, rel_file_path, func)
+		else:
+			if path == root_dir:
+				dir_path = '.'
+			else:
+				dir_path = path.replace(os.path.sep, '/')[len(root_dir) + 1:]
+			
+			if dir_path not in doc_files:
+				doc_files[dir_path] = {'i18n': {}, 'files': {}}
+			
+			doc_file = doc_files[dir_path]
+			
+			document = minidom.parse(file_path)
+			
+			title = get_unique_value(document, 'title', file_path)
+			
+			if file_name == 'index.xml':
+				# write i18n for directory
+				doc_file['i18n']['de'] = title
+				doc_file['i18n']['en'] = po_i18n[title]
+				
+				continue
+			
+			doc_file['files'][file_name] = {'de': title, 'en': po_i18n[title]}
 
 # sort by name first, then group by file (one constgroup-file can have multiple constants)
 constants.sort(key=lambda item: item['name'])
@@ -124,9 +149,23 @@ for category in categories:
 for category in categories:
 	category_i18n['en'][category] = po_i18n[category].split('/')
 
+# sort files ascending
+for dir_name in doc_files:
+	directory = doc_files[dir_name]
+	
+	dir_files = directory['files']
+	files_sorted_keys = list(dir_files.keys())
+	files_sorted_keys.sort()
+	directory['files'] = {name: dir_files[name] for name in files_sorted_keys}
+
+# sort directories ascending
+doc_files_sorted_keys = list(doc_files.keys())
+doc_files_sorted_keys.sort()
+doc_files = {name: doc_files[name] for name in doc_files_sorted_keys}
+
 # also export the current date and time into the JSON file
 # WARNING: datetime is in local timezone
-out = {'created': datetime.datetime.now().isoformat(), 'generated_from': root_dir, 'category_i18n': category_i18n, 'constants': constants, 'functions': functions}
+out = {'created': datetime.datetime.now().isoformat(), 'generated_from': root_dir, 'files': doc_files, 'category_i18n': category_i18n, 'script': {'constants': constants, 'functions': functions}}
 with open('lcdocs_summary.json', 'w') as f:
 	# disable "ensure_ascii" so UTF-8 chars are output correctly
 	f.write(json.dumps(out, ensure_ascii=False, indent='\t'))
